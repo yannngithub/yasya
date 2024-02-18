7e6a9b5de6e0afe499e388442476a7fd9ec482a1
yannngithub <yannngithub@gmail.com>
2024-02-18 12:23:34 +0700

    Add node_modules

---
+++ node_modules/ig-unduh/src/index.js
@@ -0,0 +1,36 @@
+// Author : @rom14_
+// dan folow juga ig gw @mmv.alya
+const axios = require("axios")
+const cheerio = require('cheerio')
+module.exports = instagramGetUrl = (url) =>{
+    return new Promise(async (resolve) => {
+         try {
+            let json = await (await axios.post("https://saveig.app/api/ajaxSearch", require('querystring').stringify({ q: url, t: "media", lang: "en" }), {
+               headers: {
+                  'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
+                  'Accept-Encoding': 'gzip, deflate, br',
+                  'Origin': 'https://saveig.app/en',
+                  'Referer': 'https://saveig.app/en',
+                  'Referrer-Policy': 'strict-origin-when-cross-origin',
+                  'User-Agent': 'PostmanRuntime/7.31.1'
+               }
+            })).data
+            let $ = cheerio.load(json.data)
+            let data = []
+            $('div[class="download-items__btn"]').each((i, e) => data.push({ type: $(e).find('a').attr('href').match('.jpg') ? 'image' : 'video', url: $(e).find('a').attr('href') }))
+            if (!data.length) return resolve({
+               status: false
+            }) 
+            resolve({
+               status: true,
+               data
+            })
+         } catch (e) {
+            console.log(e)
+            return resolve({
+               status: false,
+               msg: e.message
+            })
+         }
+      })
+   }
